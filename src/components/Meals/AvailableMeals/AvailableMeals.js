f574b48df5bed4ef4f9d61cb0905e0c38ae751e0
rkolev <radoslav.kolev@dxc.com>
2023-01-13 12:51:16 +0200

    Added a Meals component

---
+++ src/components/Meals/AvailableMeals/AvailableMeals.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { DUMMY_MEALS } from '../../../data/dummy-meals';
+import classes from './AvailableMeals.module.css';
+
+const AvailableMeals = () => {
+  const mealsList = DUMMY_MEALS.map(
+    meal => <li>{meal.name}</li>
+  );
+
+  return (
+    <section className={classes.meals}>
+      <ul>
+        {mealsList}
+      </ul>
+    </section>
+  );
+};
+
+export default AvailableMeals;
