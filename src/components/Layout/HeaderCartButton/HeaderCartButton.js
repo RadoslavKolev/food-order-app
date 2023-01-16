a4dea8818c12bdd4dac4ae5b0424d9c1e4139dca
rkolev <radoslav.kolev@dxc.com>
2023-01-16 22:52:05 +0200

    Finding the total amount of cart items

--- src/components/Layout/HeaderCartButton/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton/HeaderCartButton.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import CartIcon from '../../Cart/CartIcon/CartIcon';
+import CartContext from '../../../context/cart-context';
 
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+  const cartContext = useContext(CartContext);
+
+  // Finds the total amount of items in the cart
+  const numberOfCartItems = cartContext.items.reduce((currentNumber, item) => {
+    return currentNumber + item.amount
+  }, 0);
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>
+        {numberOfCartItems}
+      </span>
     </button>
   );
 };
